01ce238e1ded6133982ef676c65349e0837d8ce0
Koposova1 <Koposova1@KOPOSOVANA>
2024-04-10 09:50:48 +0300

    Добавьте файлы проекта.

---
+++ Lesson10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите градусы");        
+            int gradus = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите минуты");
+            int min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите секунды");
+            int second = Convert.ToInt32(Console.ReadLine());
+
+            radians radians = new radians(gradus, min, second);
+
+            double Radians = radians.ToRadians();
+            Console.WriteLine(radians.Gradus);
+            Console.WriteLine(Radians);
+            Console.ReadKey();
+        }        
+    }
+}
---
+++ Lesson10/radians.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10
+{
+    class radians
+    {
+        int gradus;
+        int min;
+        int second;
+
+        public int Gradus
+        { 
+            get 
+            { 
+                return gradus; 
+            } 
+            set 
+            { 
+                gradus = value; 
+            } 
+        }
+        public int Min
+        { 
+            get 
+            { 
+                return min; 
+            } 
+            set 
+            { 
+                min = value % 60; 
+                Gradus += value / 60; 
+            } 
+        }
+        public int Second
+        { 
+            get
+            {
+                return second; 
+            } 
+            set 
+            { 
+                second = value % 60; 
+                Min += value / 60; 
+            } 
+        }
+        public radians(int gradus, int min, int second)
+        {
+            Gradus = gradus;
+            Min = min;
+            Second = second;
+        }
+
+
+        public double ToRadians()
+        {
+            double Radians = (gradus + min + second) * Math.PI / 180;
+            return Radians;
+        }
+    }
+}
